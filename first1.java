67db98dea93785ab20a3a6bdc314065610f16a82
Aswini <116161968+AswiniNatesan@users.noreply.github.com>
2023-06-14 12:37:54 +0530

    Update first1.java

--- first1.java
+++ first1.java
@@ -2,5 +2,6 @@ public class first1{
 	public static void main (String[] args){
 		System.out.println("hello world");
 		System.out.println("welcome to jenkins");//newly added
+		System.out.println("new change");
 }
 }
